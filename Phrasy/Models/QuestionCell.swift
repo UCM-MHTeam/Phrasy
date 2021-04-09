5132327c80443644ed8e6aecb9ab9e291965ac33
jballo <jballona5127@gmail.com>
2021-04-09 15:33:39 -0700

    Functioning phrase label logic level 1 (with button)

--- Phrasy/Models/QuestionCell.swift
+++ Phrasy/Models/QuestionCell.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class QuestionCell: UITableViewCell {
-
+    
+//    var selectedButton:[Bool] = [false,false,false,false]
+    var selectedButton = -1
+    
+    
     @IBOutlet weak var questionLabel: UILabel!
     
     @IBOutlet weak var buttonA: UIButton!
@@ -17,13 +21,48 @@ class QuestionCell: UITableViewCell {
     @IBOutlet weak var buttonD: UIButton!
     
     @IBAction func buttonA(_ sender: Any) {
+//        if(selectedButton[0] == false){
+//            selectedButton[0] = true
+//        }else{
+//            selectedButton[0] = false
+//        }
+        selectedButton = 0
+        print(selectedButton)
+        
     }
     @IBAction func buttonB(_ sender: Any) {
+//        if(selectedButton[1] == false){
+//            selectedButton[1] = true
+//        }else{
+//            selectedButton[1] = false
+//        }
+        selectedButton = 1
+        print(selectedButton)
     }
     @IBAction func buttonC(_ sender: Any) {
+//        if(selectedButton[2] == false){
+//            selectedButton[2] = true
+//        }else{
+//            selectedButton[2] = false
+//        }
+        selectedButton = 2
+        print(selectedButton)
+        
     }
     @IBAction func buttonD(_ sender: Any) {
+//        if(selectedButton[3] == false){
+//            selectedButton[3] = true
+//        }else{
+//            selectedButton[3] = false
+//        }
+        selectedButton = 3
+        print(selectedButton)
     }
+    
+    func returnSelectedCellButton()-> Int{
+        return selectedButton
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -32,8 +71,11 @@ class QuestionCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
+    
+    
+    
+    
 
 }
--- Phrasy/ViewControllers/PhraseEngineViewController.swift
+++ Phrasy/ViewControllers/PhraseEngineViewController.swift
@@ -8,29 +8,41 @@
 import UIKit
 
 class PhraseEngineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-        
-
     
+    var questionCells:[QuestionCell] = [];
+
+    @IBOutlet weak var phraseLabel: UILabel!
     
     @IBOutlet weak var tableView: UITableView!
     
     let questionArr:[String] = ["What did they ask you?","What best describes what you’re feeling now?", "more text", "more text", "hello there"]
    
-    let buttonStatements:[[String]] = [
-        ["How are you feeling?","Another question", "More", "More Text"],
-        ["Anxious","Angry", "Worried", "Confused"],
-        ["some alone time", "some company", "to make a decision", "To take action on something"],
-        ["Give me space", "Listen to me", "Accompany me for a while", "Provide some input"]
+    @IBAction func formPhrase(_ sender: Any) {
+        self.formPhraseLabel()
+    }
     
+    let buttonStatements:[[String]] = [
+        ["A", "B", "C", "D"],
+        ["E", "F", "G", "H"],
+        ["I", "J", "K", "L"],
+        ["M", "N", "O", "P"]
     ]
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
+    
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.formPhraseLabel()
+        self.reloadInputViews()
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
@@ -38,31 +50,23 @@ class PhraseEngineViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell", for: indexPath) as! QuestionCell
-        print(indexPath.row)
-//        print(counter)
         
         cell.questionLabel.text = questionArr[indexPath.row] as? String
         
         for n in 0...3 {
             if(n == 0){
                 cell.buttonA.setTitle(buttonStatements[indexPath.row][n], for: .normal)
-//                print(cell.buttonA.titleLabel!)
             }else if(n == 1){
                 cell.buttonB.setTitle(buttonStatements[indexPath.row][n], for: .normal)
-//                print(cell.buttonB.titleLabel!)
             }else if(n == 2){
                 cell.buttonC.setTitle(buttonStatements[indexPath.row][n], for: .normal)
-//                print(cell.buttonC.titleLabel!)
             }else{
                 cell.buttonD.setTitle(buttonStatements[indexPath.row][n], for: .normal)
-//                print(cell.buttonD.titleLabel!)
             }
-            print(cell.buttonA.titleLabel!)
-            print(cell.buttonB.titleLabel!)
-            print(cell.buttonC.titleLabel!)
-            print(cell.buttonD.titleLabel!)
         }
-                
+        
+        questionCells.append(cell)
+        
         return cell
 
     }
@@ -75,5 +79,52 @@ class PhraseEngineViewController: UIViewController, UITableViewDelegate, UITable
         // Pass the selected object to the new view controller.
     }
     */
+    @objc func formPhraseLabel(){
+        //Hey <person>, I’m feeling (2) right now, and I need (3). Will you (4)?
+        var phraseBones:[String] = ["Hey ", "___",", I'm feeling ", "___"," right now, and I need ", "___", ". Will you ", "___", "?"]
+        
+        var sentence = ""
+        var chosenButtons:[String] = ["default", "default", "default", "default"]
+        for n in 0...3{
+            if(questionCells[n].returnSelectedCellButton() != -1){
+                if(questionCells[n].returnSelectedCellButton() == 0){
+                    chosenButtons[n] = (questionCells[n].buttonA.titleLabel?.text)!
+                }
+                if(questionCells[n].returnSelectedCellButton() == 1){
+                    chosenButtons[n] = (questionCells[n].buttonB.titleLabel?.text)!
+                }
+                if(questionCells[n].returnSelectedCellButton() == 2){
+                    chosenButtons[n] = (questionCells[n].buttonC.titleLabel?.text)!
+                }
+                if(questionCells[n].returnSelectedCellButton() == 3){
+                    chosenButtons[n] = (questionCells[n].buttonD.titleLabel?.text)!
+                }
+            
+        }
+        
+            if(chosenButtons[0] != "default"){
+                phraseBones[1] = chosenButtons[0]
+            }
+            if(chosenButtons[1] != "default"){
+                phraseBones[3] = chosenButtons[1]
+            }
+            if(chosenButtons[2] != "default"){
+                phraseBones[5] = chosenButtons[2]
+            }
+            if(chosenButtons[3] != "default"){
+                phraseBones[7] = chosenButtons[3]
+            }
+        }
+        
+        
+        for m in 0...(phraseBones.count-1){
+            sentence += phraseBones[m]
+        }
+        print(sentence)
+        
+        phraseLabel.text = sentence as? String
+        
+        
+    }
 
 }
