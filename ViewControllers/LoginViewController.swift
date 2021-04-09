2cb64a034bb7919c4be04d6101cfc11a2e41c502
Erick <crazynerd89@gmail.com>
2021-04-09 16:19:54 -0700

    2 Text Fields 2 Buttons

---
+++ ViewControllers/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Phrasy
+//
+//  Created by Erick Barajas on 4/7/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var SignIn: UIButton!
+    @IBOutlet weak var SignUp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
