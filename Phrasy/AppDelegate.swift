150ad70fa3c8fc6f1ef64535a0addf3513bf1d61
Erick <crazynerd89@gmail.com>
2021-04-13 18:26:46 -0700

    Parse implemented

--- Phrasy/AppDelegate.swift
+++ Phrasy/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let parseConfig = ParseClientConfiguration {
-                    $0.applicationId = "" // <- UPDATE
-                    $0.clientKey = "" // <- UPDATE
+                    $0.applicationId = "kkf3VLHWmScL5viqyhFRVoedi2eWdEL38eof2q6F" // <- UPDATE
+                    $0.clientKey = "lGinwzJ8DzS2WmlSIcqESE1jBnMCCOGGZbApTY7H" // <- UPDATE
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
