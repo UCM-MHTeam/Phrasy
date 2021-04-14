e5fde538a4440469f7b85f22f43f0e9cbd80a84f
Erick <crazynerd89@gmail.com>
2021-04-13 17:28:06 -0700

    Parse

--- ViewControllers/LoginViewController.swift
+++ ViewControllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -18,10 +19,32 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func SignIn(_ sender: Any) {
-    }
     
+    @IBAction func SignIn(_ sender: Any) {
+    
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
     @IBAction func SignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
 
