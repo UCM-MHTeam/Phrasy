0db748782897dffebbbe5c054d0c66d2c124d5dd
jballo <jballona5127@gmail.com>
2021-04-13 18:35:51 -0700

    Correct "applicationId" and "clientKey" for parse config

--- Phrasy/AppDelegate.swift
+++ Phrasy/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let parseConfig = ParseClientConfiguration {
-                    $0.applicationId = "kkf3VLHWmScL5viqyhFRVoedi2eWdEL38eof2q6F" // <- UPDATE
-                    $0.clientKey = "lGinwzJ8DzS2WmlSIcqESE1jBnMCCOGGZbApTY7H" // <- UPDATE
+                    $0.applicationId = "OVWIOYwEUbiAMH74i43HqVTekBalVTpUWVE6gIX1" // <- UPDATE
+                    $0.clientKey = "WPnNTn89a9vtwFeiJqUDLQJCATI1sIvPjVsYWwac" // <- UPDATE
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
