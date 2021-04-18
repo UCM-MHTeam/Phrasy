916295577adb5b924623ef5ca9e05f0b78694cf4
Marty <mdrnodado@gmail.com>
2021-04-18 12:26:43 -0700

    added person cell content and design

--- Phrasy/ViewControllers/HomeViewController.swift
+++ Phrasy/ViewControllers/HomeViewController.swift
@@ -7,15 +7,38 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
 
+    @IBOutlet weak var friendsView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        friendsView.dataSource = self
+        friendsView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PersonCell", for: indexPath) as! PersonCell
+        
+        cell.personNameLabel.text = "Drake B."
+        cell.personImage.image = UIImage(named: "logo")
+        
+        // Person Cell Design
+        cell.contentView.layer.cornerRadius = 24
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOpacity = 0.25
+        cell.layer.shadowOffset = CGSize(width:0,height: 2.0)
+        cell.layer.shadowRadius = 10
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
@@ -27,3 +50,4 @@ class HomeViewController: UIViewController {
     */
 
 }
+
