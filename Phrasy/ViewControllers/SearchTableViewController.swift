d3967df4f72a683bc83a551a0e8918d0fffa3bc8
jballo <jballona5127@gmail.com>
2021-04-26 03:20:14 -0700

    Working Add/Search friends
    Desperately needs cleaning up code

--- Phrasy/ViewControllers/SearchTableViewController.swift
+++ Phrasy/ViewControllers/SearchTableViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import Parse
 
 class SearchTableViewController:  UITableViewController, UISearchBarDelegate {
     
     let data = ["Apples", "Oranges", "Pears", "Bananas", "Plums"]
     var filteredData: [String]!
+
+    var dataUsers = [PFObject]()
+    var filteredUsers = [PFObject]()
 
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -18,9 +22,21 @@ class SearchTableViewController:  UITableViewController, UISearchBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchBar.delegate = self
+        let query = PFQuery(className: "_User")
         
-        filteredData = data
+        query.findObjectsInBackground { (users, error) -> Void in
+            if users != nil{
+                self.dataUsers = users!
+                self.filteredUsers = self.dataUsers
+                self.tableView.reloadData()         //keep?
+                
+            }else{
+                print("failure to retreive data")
+            }
+        }
+        
+        searchBar.delegate = self
+        filteredUsers = dataUsers
 
     }
 
@@ -33,14 +49,30 @@ class SearchTableViewController:  UITableViewController, UISearchBarDelegate {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return filteredData.count
+//        return filteredData.count
+        return filteredUsers.count
+//        return dataUsers.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+//        print("Data users: \(dataUsers.count)")
+//        print("Filtered users: \(filteredUsers.count)")
+//        let person = dataUsers[indexPath.row] as! PFUser
         
-        cell.textLabel?.text = filteredData[indexPath.row]
+//        cell.textLabel?.text = person["username"] as? String
+//        cell.textLabel?.text = filteredData[indexPath.row]
+        
+//        let person = dataUsers[indexPath.row] as! PFUser
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell")! as! UserCell
+//        cell.userName.text = person["username"] as? String
+        print("Hi")
+        print("Filtered users: \(filteredUsers.count)")
+        let person = filteredUsers[indexPath.row] as! PFUser
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell")! as! UserCell
+        cell.userName.text = person["username"] as? String
+        cell.userID = person.objectId!
         
         return cell
     }
@@ -50,18 +82,28 @@ class SearchTableViewController:  UITableViewController, UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         filteredData = []
+        filteredUsers = []
         
         if searchText == ""{
             filteredData = data
+            filteredUsers = dataUsers
         }
         else {
             for fruit in data{
+                
                 
                 if fruit.lowercased().contains(searchText.lowercased()) {
                     
                     filteredData.append(fruit)
                 }
             }
+            for members in dataUsers{
+                let name = members["username"] as? String
+                if name!.lowercased().contains(searchText.lowercased()){
+                    filteredUsers.append(members)
+                }
+            }
+            
         }
         self.tableView.reloadData()
     }
