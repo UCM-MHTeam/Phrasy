f4089c1865f0786c790a5fa36c0ddcd607182bdf
Marty <mdrnodado@gmail.com>
2021-04-26 22:09:19 -0700

    added gradient to settings

--- Phrasy/ViewControllers/SettingsViewController.swift
+++ Phrasy/ViewControllers/SettingsViewController.swift
@@ -12,6 +12,7 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setGradient()
 
         // Do any additional setup after loading the view.
     }
@@ -30,14 +31,8 @@ class SettingsViewController: UIViewController {
         })
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setGradient() {
+        gradient.frame = view.bounds
+        self.view.layer.insertSublayer(gradient, at: 0)
     }
-    */
-
 }
